6c98daf20bcc4a932a7c0c5c966beb5ef182f2c7
Andres Dorado <andres.dorado90@gmail.com>
2022-07-03 16:23:38 +0200

    split the main page component in multiple children components

--- 01-Bases/src/app/components/dbz/dbz.module.ts
+++ 01-Bases/src/app/components/dbz/dbz.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 import { FormsModule } from '@angular/forms';
+import { FightersComponent } from './fighters/fighters.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, FightersComponent],
   exports: [MainPageComponent],
   imports: [CommonModule, FormsModule],
 })
---
+++ 01-Bases/src/app/components/dbz/fighters/fighters.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { MainPageComponent } from '../main-page/main-page.component';
+
+@Component({
+  selector: 'app-fighters',
+  templateUrl: './fighters.component.html',
+})
+export class FightersComponent extends MainPageComponent {}
