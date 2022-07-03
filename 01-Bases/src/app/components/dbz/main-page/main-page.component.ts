8d172f8123ba6828f6f87806340af922443e67ec
Andres Dorado <andres.dorado90@gmail.com>
2022-07-03 16:10:21 +0200

    Use of ngModel for doble binding and forms interaction

--- 01-Bases/src/app/components/dbz/main-page/main-page.component.ts
+++ 01-Bases/src/app/components/dbz/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-class Fighter {
-  constructor(public name: string, public power: number) {
-    this.name = name;
-    this.power = power;
-  }
+interface Fighter {
+  name: string;
+  power: number;
+}
+class Fighter implements Fighter {
+  constructor(public name: string, public power: number) {}
 }
 
 @Component({
@@ -14,12 +15,18 @@ class Fighter {
 })
 export class MainPageComponent implements OnInit {
   public fighters: Fighter[] = [];
+  newFighter: Fighter = { name: '', power: 0 };
   ngOnInit(): void {
-    this.fighters.push(new Fighter('Goku', 1800));
-    this.fighters.push(new Fighter('Vegeta', 1200));
-    this.fighters.push(new Fighter('Krilin', 300));
+    this.fighters.push(new Fighter('Goku', 18000));
+    this.fighters.push(new Fighter('Vegeta', 12000));
   }
   addFighter = () => {
-    console.log();
+    const { name, power } = this.newFighter;
+    debugger;
+    if (name.trim().length === 0 || power < 0) {
+      return;
+    }
+    this.fighters.push(new Fighter(name, power));
+    this.newFighter = { name: '', power: 0 };
   };
 }
