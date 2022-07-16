974e5d60d8183de211a267b6c6d49cc38d5effa6
Andres Dorado <andres.dorado90@gmail.com>
2022-07-16 15:56:36 +0200

    Project structuration

---
+++ 03-countryApp/src/app/country/pages/by-capital/by-capital.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'con-by-capital',
+  templateUrl: './by-capital.component.html',
+  styleUrls: ['./by-capital.component.scss']
+})
+export class ByCapitalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ 03-countryApp/src/app/country/pages/by-region/by-region.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'con-by-region',
+  templateUrl: './by-region.component.html',
+  styleUrls: ['./by-region.component.scss']
+})
+export class ByRegionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
