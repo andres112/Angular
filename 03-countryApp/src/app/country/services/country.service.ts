438e93f29717e1ddf9566289775339446fff50f4
Andres Dorado <andres.dorado90@gmail.com>
2022-08-01 20:51:23 +0200

    Optimization of http requests filtering data to be retrieved

--- 03-countryApp/src/app/country/services/country.service.ts
+++ 03-countryApp/src/app/country/services/country.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
@@ -8,23 +8,33 @@ import { Country } from '../interfaces/country.interface';
 })
 export class CountryService {
   private API_URL: string = 'https://restcountries.com/v2';
+  private httpParams = new HttpParams().set(
+    'fields',
+    'name,capital,flag,population,alpha2Code'
+  );
 
   constructor(private http: HttpClient) {}
 
   // This function return an observable
   searchCountry = (country: string): Observable<Country[]> => {
-    return this.http.get<Country[]>(`${this.API_URL}/name/${country}`);
+    return this.http.get<Country[]>(`${this.API_URL}/name/${country}`, {
+      params: this.httpParams,
+    });
   };
 
   searchCapital = (capital: string): Observable<Country[]> => {
-    return this.http.get<Country[]>(`${this.API_URL}/capital/${capital}`);
-  }
+    return this.http.get<Country[]>(`${this.API_URL}/capital/${capital}`, {
+      params: this.httpParams,
+    });
+  };
 
   searchCountryByCode = (code: string): Observable<Country> => {
     return this.http.get<Country>(`${this.API_URL}/alpha/${code}`);
-  }
+  };
 
   searchRegion = (region: string): Observable<Country[]> => {
-    return this.http.get<Country[]>(`${this.API_URL}/region/${region}`);
-  }
+    return this.http.get<Country[]>(`${this.API_URL}/region/${region}`, {
+      params: this.httpParams,
+    });
+  };
 }
