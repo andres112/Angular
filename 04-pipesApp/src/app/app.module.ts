6c3a25960f6596e4dde31a859f74392f3d218068
Andres Dorado <andres.dorado90@gmail.com>
2022-09-03 20:35:02 +0200

    global change of locale

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 
@@ -6,10 +6,15 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { SalesModule } from './sales/sales.module';
 
+// change the app locale
+import spanish from '@angular/common/locales/es-CO';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(spanish);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRouterModule, SharedModule, SalesModule],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-CO' }], // for global change of locale
   bootstrap: [AppComponent],
 })
 export class AppModule {}
