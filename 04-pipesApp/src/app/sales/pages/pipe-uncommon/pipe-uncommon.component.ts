35bd54f3bd083ad4088bf3e975c4bb0fd46f66c9
Andres Dorado <andres.dorado90@gmail.com>
2022-09-04 16:11:53 +0200

    implementation of i18nSelect pipe

--- 04-pipesApp/src/app/sales/pages/pipe-uncommon/pipe-uncommon.component.ts
+++ 04-pipesApp/src/app/sales/pages/pipe-uncommon/pipe-uncommon.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pipe-uncommon',
   templateUrl: './pipe-uncommon.component.html',
-  styleUrls: ['./pipe-uncommon.component.scss']
+  styleUrls: ['./pipe-uncommon.component.scss'],
 })
-export class PipeUncommonComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PipeUncommonComponent {
+  public people: { name: string; gender: string }[] = [
+    {
+      name: 'Andres',
+      gender: 'male',
+    },
+    {
+      name: 'Tatjana',
+      gender: 'female',
+    },
+  ];
+  public mapping = {
+    male: 'him',
+    female: 'her',
+  };
 }
