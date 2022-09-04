2c32c37e78717b8d55e4c5d006879eb6d1595147
Andres Dorado <andres.dorado90@gmail.com>
2022-09-04 16:39:02 +0200

    i18nPlural pipe for pluralization consideration

--- 04-pipesApp/src/app/sales/pages/pipe-uncommon/pipe-uncommon.component.ts
+++ 04-pipesApp/src/app/sales/pages/pipe-uncommon/pipe-uncommon.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./pipe-uncommon.component.scss'],
 })
 export class PipeUncommonComponent {
+  // i18nSelect pipe data
   public people: { name: string; gender: string }[] = [
     {
       name: 'Andres',
@@ -19,5 +20,16 @@ export class PipeUncommonComponent {
   public mapping = {
     male: 'him',
     female: 'her',
+  };
+  // i18nPlural pipe data
+  public clients = [
+    { people: [] },
+    { people: ['Maria'] },
+    { people: ['Pedro', 'Jesus','Maria'] },
+  ];
+  public pluralMap = {
+    '=0': 'Any client is waiting',
+    '=1': 'There is # client waiting',
+    other: 'There are # clients waiting',
   };
 }
