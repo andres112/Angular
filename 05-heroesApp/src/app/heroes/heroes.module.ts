3ad62c242347988c16f27c43eba1d228ca2aec6e
Andres Dorado <andres.dorado90@gmail.com>
2022-11-01 17:46:21 +0100

    Heroes module with children routes and lazy loading

--- 05-heroesApp/src/app/heroes/heroes.module.ts
+++ 05-heroesApp/src/app/heroes/heroes.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeroesRoutingModule } from './heroes-routing.module';
+
 import { AddComponent } from './pages/add/add.component';
 import { SearchComponent } from './pages/search/search.component';
 import { HeroComponent } from './pages/hero/hero.component';
@@ -12,8 +14,8 @@ import { ListComponent } from './pages/list/list.component';
     SearchComponent,
     HeroComponent,
     HomeComponent,
-    ListComponent
+    ListComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, HeroesRoutingModule],
 })
 export class HeroesModule {}
--- 05-heroesApp/src/app/shared/shared.module.ts
+++ 05-heroesApp/src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [ErrorPageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
