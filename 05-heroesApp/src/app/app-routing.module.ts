88ae059d18cd4d7be8f027b083f2acb92fbed4c5
Andres Dorado <andres.dorado90@gmail.com>
2022-11-01 16:15:03 +0100

    children routes of auth module, with lazy loading in main

---
+++ 05-heroesApp/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+const routes: Routes = [
+  // Path with modularization load all children routes
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  { path: '404', component: ErrorPageComponent },
+  { path: '**', redirectTo: '404' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
