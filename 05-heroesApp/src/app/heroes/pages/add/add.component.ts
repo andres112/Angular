49c95a3b6e46d6151ab5464208f6a6aa007a9307
Andres Dorado <andres.dorado90@gmail.com>
2022-11-05 13:07:26 +0100

    add hero form creation

--- 05-heroesApp/src/app/heroes/pages/add/add.component.ts
+++ 05-heroesApp/src/app/heroes/pages/add/add.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/heroes.model';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  public hero = {} as Hero;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public onSave(): void {
+    if (this.hero.superhero.trim().length === 0) {
+      return;
+    }
   }
-
 }
