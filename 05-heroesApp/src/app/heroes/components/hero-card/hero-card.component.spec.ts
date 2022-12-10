c1f1b09cf4b8d0559ab2ade1ce77b690bc471b53
Andres Dorado <andres.dorado90@gmail.com>
2022-12-10 17:56:41 +0100

    some testing changes in hero card component

--- 05-heroesApp/src/app/heroes/components/hero-card/hero-card.component.spec.ts
+++ 05-heroesApp/src/app/heroes/components/hero-card/hero-card.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Hero } from '../../interfaces/heroes.model';
 
 import { HeroCardComponent } from './hero-card.component';
+import { ImagePipe } from '../../pipes/image.pipe';
 
 describe('HeroCardComponent', () => {
   let component: HeroCardComponent;
@@ -8,7 +11,7 @@ describe('HeroCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeroCardComponent ]
+      declarations: [ HeroCardComponent, ImagePipe ]
     })
     .compileComponents();
 
@@ -19,5 +22,23 @@ describe('HeroCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the hero name if the hero input is provided', () => {
+    // Define the mock hero
+    const mockHero: Hero = {
+      superhero: 'Superman',
+    } as Hero;
+    // Set the hero input
+    component.hero = mockHero;
+
+    // Trigger a change detection cycle
+    fixture.detectChanges();
+
+    // Get the hero name element
+    const heroName = fixture.debugElement.query(By.css('.hero-name'));
+
+    // Verify that the hero name is displayed
+    expect(heroName.nativeElement.innerHTML).toBe('Superman');
   });
 });
