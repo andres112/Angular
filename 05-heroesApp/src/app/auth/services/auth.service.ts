a84034496c9d2c4e2940b2bbaea3373bb6656b98
Andres Felipe Dorado <andres.dorado90@gmail.com>
2023-01-04 13:03:09 +0100

    present information from login on screen

--- 05-heroesApp/src/app/auth/services/auth.service.ts
+++ 05-heroesApp/src/app/auth/services/auth.service.ts
@@ -2,17 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { User } from '../interfaces/user.model';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-
   private baseURL: string = environment.baseUrl;
+  private _auth: User | undefined;
+
+  get auth(): User {
+    return { ...this._auth } as User;
+  }
   constructor(private http: HttpClient) {}
 
-  public login(){
-    return this.http.get<User>(this.baseURL+'usuarios/2');
+  public login() {
+    return this.http
+      .get<User>(this.baseURL + 'usuarios/2')
+      .pipe(tap((resp) => (this._auth = resp)));
   }
-
 }
