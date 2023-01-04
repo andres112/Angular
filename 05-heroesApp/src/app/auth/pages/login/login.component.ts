767c9644ff0008ed513e7454677fade3455591a1
Andres Felipe Dorado <andres.dorado90@gmail.com>
2023-01-04 11:34:31 +0100

    simple login, logout interaction. Pending: functionality

--- 05-heroesApp/src/app/auth/pages/login/login.component.ts
+++ 05-heroesApp/src/app/auth/pages/login/login.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public login(){
+    console.log('login');
+    this.router.navigate(['/heroes']);
   }
-
 }
