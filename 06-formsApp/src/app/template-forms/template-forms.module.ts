b3746a81b2cb3346744e6d92228c1eb18ba62d2f
Andres Felipe Dorado <andres.dorado90@gmail.com>
2023-01-06 18:28:14 +0100

    creation of modules and children routes

--- 06-formsApp/src/app/template-forms/template-forms.module.ts
+++ 06-formsApp/src/app/template-forms/template-forms.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TemplateFormsRoutingModule } from './template-forms-routing.module';
+import { SwitchesComponent } from './switches/switches.component';
+import { BasicsComponent } from './basics/basics.component';
+import { DynamicsComponent } from './dynamics/dynamics.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SwitchesComponent,
+    BasicsComponent,
+    DynamicsComponent
+  ],
   imports: [
     CommonModule,
     TemplateFormsRoutingModule
