fa429b59968d749c781b3f7f844c90e2e579d2c4
Andres Felipe Dorado <andres.dorado90@gmail.com>
2023-01-07 13:09:04 +0100

    first implementation of template form

--- 06-formsApp/src/app/template-forms/basics/basics.component.ts
+++ 06-formsApp/src/app/template-forms/basics/basics.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
   templateUrl: './basics.component.html',
-  styleUrls: ['./basics.component.scss']
+  styleUrls: ['./basics.component.scss'],
 })
 export class BasicsComponent {
-
+  public save(data: NgForm): void {
+    console.log(data.value);
+  }
 }
--- 06-formsApp/src/app/template-forms/template-forms.module.ts
+++ 06-formsApp/src/app/template-forms/template-forms.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TemplateFormsRoutingModule } from './template-forms-routing.module';
 import { SwitchesComponent } from './switches/switches.component';
@@ -15,6 +16,7 @@ import { DynamicsComponent } from './dynamics/dynamics.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     TemplateFormsRoutingModule
   ]
 })
