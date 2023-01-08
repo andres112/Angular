ce612a5e08c43ea3aa160ca8cfe55d584456adf6
Andres Felipe Dorado <andres.dorado90@gmail.com>
2023-01-08 15:26:31 +0100

    reset form, reseting also the form validations

--- 06-formsApp/src/app/template-forms/basics/basics.component.ts
+++ 06-formsApp/src/app/template-forms/basics/basics.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -16,19 +16,38 @@ export class BasicsComponent {
     );
   }
 
-  public validPrice():boolean {
+  public validPrice(): boolean {
     return (
       this.myForm?.value.price < 0 ||
-      this.myForm?.controls['price']?.invalid &&
-      this.myForm?.controls['price']?.touched
+      (this.myForm?.controls['price']?.invalid &&
+        this.myForm?.controls['price']?.touched)
     );
   }
 
+  public validStock(): ValidationErrors | null {
+    if (
+      this.myForm?.controls['stock']?.invalid &&
+      this.myForm?.controls['stock']?.touched
+    ) {
+      return this.myForm?.controls['stock']?.errors;
+    }
+    return null;
+  }
+
   public save(): void {
-    if(this.myForm.invalid) {
+    if (this.myForm.invalid) {
       this.myForm.control.markAllAsTouched();
       return;
     }
     console.log(this.myForm);
+    this.resetForm();
+  }
+
+  public resetForm(): void {
+    this.myForm.resetForm({
+      product: '',
+      price: 0,
+      stock: 0,
+    });
   }
 }
