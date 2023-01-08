0b406c46b79fa8d1eb983b3708b688213b9fab9e
Andres Felipe Dorado <andres.dorado90@gmail.com>
2023-01-08 16:03:02 +0100

    Dynamic template form structure created

--- 06-formsApp/src/app/template-forms/dynamics/dynamics.component.ts
+++ 06-formsApp/src/app/template-forms/dynamics/dynamics.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamics',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./dynamics.component.scss']
 })
 export class DynamicsComponent {
+  @ViewChild('templateDynamicForm') myForm!: NgForm;
+
+  public validName(): boolean {
+    return (
+      this.myForm?.controls['name']?.invalid &&
+      this.myForm?.controls['name']?.touched
+    );
+  }
+
+  public save(): void {
+    console.log('Save');
+  }
 
 }
