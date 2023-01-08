50b4acc1eff661dfff2b713e9760d4afa7dab264
Andres Felipe Dorado <andres.dorado90@gmail.com>
2023-01-08 16:54:26 +0100

    Add elements to form dynamically and double bind with ngModel

--- 06-formsApp/src/app/template-forms/dynamics/dynamics.component.ts
+++ 06-formsApp/src/app/template-forms/dynamics/dynamics.component.ts
@@ -1,13 +1,28 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
+interface Person {
+  name: string;
+  favorites: Favorite[];
+}
+interface Favorite {
+  id: number;
+  name: string;
+  selected: boolean;
+}
 
 @Component({
   selector: 'app-dynamics',
   templateUrl: './dynamics.component.html',
-  styleUrls: ['./dynamics.component.scss']
+  styleUrls: ['./dynamics.component.scss'],
 })
-export class DynamicsComponent {
+export class DynamicsComponent implements OnInit {
   @ViewChild('templateDynamicForm') myForm!: NgForm;
+  public person: Person = {} as Person;
+
+  public ngOnInit(): void {
+    this.person.favorites=[];
+  }
 
   public validName(): boolean {
     return (
@@ -16,8 +31,17 @@ export class DynamicsComponent {
     );
   }
 
+  public addFavorite(): void {
+    this.person.favorites.push({
+      id: this.person.favorites.length + 1,
+      name: this.myForm?.value.favorite,
+      selected: false,
+    });
+    // reset the input only
+    this.myForm?.controls['favorite']?.reset();
+  }
+
   public save(): void {
     console.log('Save');
   }
-
 }
