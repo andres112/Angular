b9213a1e1418afa242839cefc013398a12d9988c
Andres Felipe Dorado <andres.dorado90@gmail.com>
2023-02-05 16:01:06 +0100

    Use of valueChanges observable and binding to object

--- 06-formsApp/src/app/reactive-forms/switches/switches.component.ts
+++ 06-formsApp/src/app/reactive-forms/switches/switches.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styleUrls: ['./switches.component.scss']
+  styleUrls: ['./switches.component.scss'],
 })
-export class SwitchesComponent {
+export class SwitchesComponent implements OnInit {
+  public myForm: FormGroup = this.fb.group({
+    gender: ['m', Validators.required],
+    notifications: [, Validators.required],
+    terms: [, Validators.requiredTrue],
+  });
+  
 
+  public person = {
+    gender: 'a',
+    notifications: true,
+  };
+
+  constructor(private fb: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.myForm.reset({
+      ...this.person,
+    });
+
+    // monitor the form changes to update the person object
+    this.myForm.valueChanges.subscribe((form) => {
+      delete form.terms;
+      this.person = form;
+    });
+  }
+
+  public save(): void {
+    const formValue = { ...this.myForm.value };
+    delete formValue.terms;
+    this.person = formValue;
+    this.myForm.reset();
+  }
 }
