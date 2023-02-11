985ea1fde627a62210861a6a8a43d8d40ebbf69f
Andres Felipe Dorado <andres.dorado90@gmail.com>
2023-02-11 09:58:46 +0100

    components for login and register created

---
+++ 06-formsApp/src/app/auth/auth.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [CommonModule, AuthRoutingModule],
+})
+export class AuthModule {}
