40e4555de464bbd2791baac3116051bbd957416b
Andres Felipe Dorado <andres.dorado90@gmail.com>
2023-02-11 12:02:19 +0100

    form builder for register component created

--- 06-formsApp/src/app/auth/auth.module.ts
+++ 06-formsApp/src/app/auth/auth.module.ts
@@ -4,12 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [CommonModule, AuthRoutingModule],
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
--- 06-formsApp/src/app/auth/pages/register/register.component.ts
+++ 06-formsApp/src/app/auth/pages/register/register.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
 
+  public registerForm = this.fb.group({
+    name: ['', [Validators.required]],
+    email: [''],
+    password: [''],
+    repeatPassword: [''],
+  });
+
+  constructor(private fb: FormBuilder) {}
 }
