c6b7dd42745502871925162f03c7b9c4a9974910
Andres Felipe Dorado <andres.dorado90@gmail.com>
2023-03-19 14:23:14 +0100

    feat: creation of first select and reactive form

--- 07-selectors/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 07-selectors/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
-  styleUrls: ['./selector-page.component.scss']
+  styleUrls: ['./selector-page.component.scss'],
 })
 export class SelectorPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    region: '',
+  });
 
+  // TODO: Change for response from restAPI
+  public regions: string[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
+
+  constructor(private fb: FormBuilder) {}
+
+  public save(): void {
+    console.log(this.myForm.value);
+  }
 }
