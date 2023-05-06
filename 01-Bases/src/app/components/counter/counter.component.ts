1115f4cd49eeebf44900ac98bf9cf9ac97c985fd
Andres Felipe Dorado <andres.dorado90@gmail.com>
2023-05-06 20:03:32 +0200

    chore: include auto counter in base project

--- 01-Bases/src/app/components/counter/counter.component.ts
+++ 01-Bases/src/app/components/counter/counter.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'; // Import the Component decorator from the @angular/core library.
+import { BehaviorSubject, Subscription, interval } from 'rxjs';
 
 // @Component is a decorator function that takes a metadata object as its only argument.
 @Component({
@@ -7,10 +8,25 @@ import { Component } from '@angular/core'; // Import the Component decorator fro
 })
 // Export the class AppComponent.
 export class CounterComponent {
+  public autoStatus = false;
   public title: string = 'Counter app';
   accumulatorNumber: number = 0;
 
+  private interval = interval(500);
+  private intervalSubs!: Subscription;
+
   public modifyCounter(value: number): void {
     this.accumulatorNumber += value;
+  }
+
+  public autoToggler(): void {
+    this.autoStatus = !this.autoStatus;
+    if (this.autoStatus) {
+      this.intervalSubs = this.interval.subscribe(() => {
+        this.modifyCounter(1);
+      });
+    } else {
+      this.intervalSubs.unsubscribe();
+    }
   }
 }
